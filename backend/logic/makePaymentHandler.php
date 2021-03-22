8dc69024b3b1ac6881db6a768471e1bedc04bdba
ZTF666 <ZTF666@protonmail.ch>
2021-03-22 16:26:35 +0100

    :heart:

--- backend/logic/makePaymentHandler.php
+++ backend/logic/makePaymentHandler.php
@@ -4,11 +4,15 @@
 
 include('../classes/DAO.php');
 // new instance
+$myDate=date("Y/m/d");
+$Year = explode("/", $myDate);
+// echo  $Year[0];
+
 $dao = new DAO();
 // getting data from form
 $cnie   = $_POST['cnie_member'];
 $cnie_admin = $_POST['cnie_admin'];
-$month   = $_POST['month'];
+$month   = $_POST['month']."-".$Year[0];
 $amount   = $_POST['amount'];
 $notes   = $_POST['notes'];
 
